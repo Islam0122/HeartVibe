0dbc5adf47bb2f276c7a0cc95af25e6ebd4081be
islam0122 <duishobaevislam01@gmaul.com>
2025-03-28 00:48:03 +0600

    Add project structure with base settings and initial configuration

---
+++ config/settings/development.py
@@ -0,0 +1,27 @@
+from .base import *
+
+DEBUG = True
+
+INSTALLED_APPS += [
+    'debug_toolbar',  
+]
+
+MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
